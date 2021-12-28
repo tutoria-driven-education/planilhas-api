62a7429e15d5ef3f01e85958fb6ac8139bf0ce6b
Leo <celso@respondeai.com.br>
2021-12-28 15:31:23 -0300

    feature: add routes for auth with gogole; remove axios from dependecies

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,9 @@
 import * as fs from 'fs/promises'
-import readline from 'readline'
 import {google} from 'googleapis'
 
-const SCOPES = ['https://www.googleapis.com/auth/drive','https://spreadsheets.google.com/feeds'];
-const TOKEN_PATH = 'token.json';
 
-export async function authorize() {
+async function getOauth(){
   let credentials
-  let token;
   try {
     credentials =  JSON.parse(await fs.readFile('credentials.json'));
   } catch (error) {
@@ -18,44 +14,36 @@ export async function authorize() {
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
-  try {
-    token =  JSON.parse(await fs.readFile(TOKEN_PATH))
-            
-  } catch (error) {
-    console.log("Gerando token")
-    token = getAccessToken(oAuth2Client);
-    
-  }
-  oAuth2Client.setCredentials(token);
   return oAuth2Client
 }
 
-
-export function getAccessToken(oAuth2Client) {
-  let newToken;
-  const authUrl = oAuth2Client.generateAuthUrl({
+export async function getLinkToken(){
+  const SCOPES = ['https://www.googleapis.com/auth/drive','https://spreadsheets.google.com/feeds'];
+  const oAuth = await getOauth()
+  const authUrl = oAuth.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
   });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      
-      try {
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-        console.log('Token stored to', TOKEN_PATH);
-        newToken = token
-      } catch (error) {
-        return console.error(err);
-      }
-      
-    });
-  });
-  return newToken
+
+  return authUrl;
 }
+
+export async function getTokenGoogle(code){
+  const oAuth = await getOauth()
+  try{
+    const request = await (oAuth.getToken(code));
+    return request.tokens
+  }
+  catch(err){
+    console.log("Error in search token")
+  }
+}
+
+export async function authorize(token) {
+  const oAuth = await getOauth()
+  
+  oAuth.setCredentials(token);
+
+  return oAuth
+}
+
