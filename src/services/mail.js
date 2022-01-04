7a0c29e5324a16ebd534c22e6fa2aff0e1514993
Yann Melo Eduardo <81389063+Liaess@users.noreply.github.com>
2022-01-04 11:11:25 -0300

    feat: add email template (#3)
    LGTM

--- src/services/mail.js
+++ src/services/mail.js
@@ -1,4 +1,5 @@
 import NodeMailer from "nodemailer";
+import mailTemplate from "../templates/mail.js"
 
 export default function sendStudentMail(studentName, studentEmail, sheetId) {
     const mail = NodeMailer.createTransport({
@@ -8,16 +9,12 @@ export default function sendStudentMail(studentName, studentEmail, sheetId) {
         pass: process.env.EMAIL_PASSWORD
         }
     });
-
+    const template = mailTemplate(studentName, sheetId)
     var mailOptions = {
         from: process.env.EMAIL_USERNAME,
         to: studentEmail,
         subject: `Olá ${studentName}! Sua planilha individual de presença está pronta`,
-        html: `
-        <h1> Olá ${studentName}</h1>
-
-        <span>Sua planilha pode ser vista <a href="https://docs.google.com/spreadsheets/d/${sheetId}/">aqui</a></span>
-        `
+        html: template
     }
 
     return mail.sendMail(mailOptions, (error, _info)=>{
