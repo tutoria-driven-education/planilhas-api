36ed6435cb55dd1eefc093dd6796f263329f863c
Celso <celsolnv27@gmail.com>
2022-04-06 22:52:38 -0300

    feat: change extension all files js

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app.js";
 
 const port = process.env.PORT || 4000;
+
 app.listen(port, () => {
-  console.log(`server runing in port ${port}`);
+  console.log(`server running in port ${port}`);
 });
