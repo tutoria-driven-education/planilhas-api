a9ff13c6ba7506e6934686f69b4b54500c442e01
Celso <celsolnv27@gmail.com>
2022-04-07 08:40:40 -0300

    feat: refactor drive and init refactor sheet

--- src/services/google/drive.ts
+++ src/services/google/drive.ts
@@ -1,3 +1,4 @@
+import { OAuth2Client } from "google-auth-library";
 import {
   ICreateFolderParams,
   IUploadSpreadsheetParams,
@@ -8,116 +9,105 @@ import {
 import { createReadStream } from "fs";
 import { google } from "googleapis";
 
-export async function uploadSpreadsheet({
-  auth,
-  fileNameInDrive,
-  path,
-  folderId,
-}: IUploadSpreadsheetParams) {
-  const drive = google.drive({ version: "v3", auth });
+export class Drive {
+  private drive;
 
-  try {
-    const request = await Promise.resolve(
-      drive.files.create({
-        requestBody: {
-          name: fileNameInDrive,
-          mimeType: "application/vnd.google-apps.spreadsheet",
-          parents: [folderId],
-        },
-        media: {
-          body: createReadStream(path),
-          mimeType: "application/vnd.ms-excel",
-        },
-        fields: "id",
-      })
-    );
-
-    return request.data.id;
-  } catch (error) {
-    throw new Error(`Error in upload spreadsheet: ${error?.message} `);
+  constructor(auth: OAuth2Client) {
+    this.drive = google.drive({ version: "v3", auth });
   }
-}
 
-export async function createFolder({ auth, folderName }: ICreateFolderParams) {
-  const drive = google.drive({ version: "v3", auth });
+  async uploadSpreadsheet({
+    fileNameInDrive,
+    path,
+    folderId,
+  }: IUploadSpreadsheetParams) {
+    try {
+      const request = await Promise.resolve(
+        this.drive.files.create({
+          requestBody: {
+            name: fileNameInDrive,
+            mimeType: "application/vnd.google-apps.spreadsheet",
+            parents: [folderId],
+          },
+          media: {
+            body: createReadStream(path),
+            mimeType: "application/vnd.ms-excel",
+          },
+          fields: "id",
+        })
+      );
 
-  try {
-    const request = await Promise.resolve(
-      drive.files.create({
-        requestBody: {
-          name: folderName,
-          mimeType: "application/vnd.google-apps.folder",
-        },
-        fields: "id",
-      })
-    );
-    return request.data.id;
-  } catch (error) {
-    throw new Error(`Error in create folder: ${error?.message} `);
+      return request.data.id;
+    } catch (error) {
+      throw new Error(`Error in upload spreadsheet: ${error?.message} `);
+    }
   }
-}
 
-export async function copySpreadsheet({
-  auth,
-  id,
-  folderId,
-  nameFile,
-}: ICopySpreadsheetParams) {
-  const drive = google.drive({ version: "v3", auth });
-
-  try {
-    const request = await Promise.resolve(
-      drive.files.copy({
-        fileId: id,
-        requestBody: {
-          parents: [folderId],
-          name: nameFile,
-          mimeType: "application/vnd.google-apps.spreadsheet",
-        },
-      })
-    );
-    return request.data.id;
-  } catch (error) {
-    throw new Error(`Error in copy file ${error?.message}`);
+  async createFolder({ folderName }: ICreateFolderParams) {
+    try {
+      const request = await Promise.resolve(
+        this.drive.files.create({
+          requestBody: {
+            name: folderName,
+            mimeType: "application/vnd.google-apps.folder",
+          },
+          fields: "id",
+        })
+      );
+      return request.data.id;
+    } catch (error) {
+      throw new Error(`Error in create folder: ${error?.message} `);
+    }
   }
-}
 
-export async function updatePermissionFileForAnyoneReader({
-  auth,
-  id,
-}: IUpdatePermissionFileForAnyoneReaderParams) {
-  const drive = google.drive({ version: "v3", auth });
-
-  try {
-    const request = await Promise.resolve(
-      drive.permissions.create({
-        fileId: id,
-        requestBody: {
-          type: "anyone",
-          role: "reader",
-        },
-        fields: "id",
-      })
-    );
-    return request.data.id;
-  } catch (error) {
-    throw new Error(`Error in copy file ${error?.message}`);
+  async copySpreadsheet({ id, folderId, nameFile }: ICopySpreadsheetParams) {
+    try {
+      const request = await Promise.resolve(
+        this.drive.files.copy({
+          fileId: id,
+          requestBody: {
+            parents: [folderId],
+            name: nameFile,
+            mimeType: "application/vnd.google-apps.spreadsheet",
+          },
+        })
+      );
+      return request.data.id;
+    } catch (error) {
+      throw new Error(`Error in copy file ${error?.message}`);
+    }
   }
-}
 
-export async function getFilesIdsInsideFolder({
-  auth,
-  folderId,
-}: IGetFilesIdsInsideFolder) {
-  const drive = google.drive({ version: "v3", auth });
-  try {
-    return await Promise.resolve(
-      drive.files.list({
-        fields: "files(id, name)",
-        q: `'${folderId}' in parents and name contains 'Controle de Presença'`,
-      })
-    );
-  } catch (error) {
-    throw new Error(`Error getting all id's inside folder ${error?.message}`);
+  async updatePermissionFileForAnyoneReader({
+    id,
+  }: IUpdatePermissionFileForAnyoneReaderParams) {
+    try {
+      const request = await Promise.resolve(
+        this.drive.permissions.create({
+          fileId: id,
+          requestBody: {
+            type: "anyone",
+            role: "reader",
+          },
+          fields: "id",
+        })
+      );
+      return request.data.id;
+    } catch (error) {
+      throw new Error(`Error in copy file ${error?.message}`);
+    }
+  }
+
+  async getFilesIdsInsideFolder({ folderId }: IGetFilesIdsInsideFolder) {
+    try {
+      return await Promise.resolve(
+        this.drive.files.list({
+          fields: "files(id, name)",
+          q: `'${folderId}' in parents and name contains 'Controle de Presença'`,
+        })
+      );
+    } catch (error) {
+      throw new Error(`Error getting all id's inside folder ${error?.message}`);
+    }
   }
 }
---
+++ src/services/google/sheet.ts
@@ -0,0 +1,8 @@
+import { OAuth2Client } from "google-auth-library";
+
+export class Sheet{
+  private sheet;
+  constructor(auth:OAuth2Client){
+    this.sheet = 
+  }
+}
