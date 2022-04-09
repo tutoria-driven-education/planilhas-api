37e02e1fad9412574a9511c9d68e8091729dcb41
Celso <celsolnv27@gmail.com>
2022-04-09 14:40:20 -0300

    fix: add recursion in mail

--- src/services/mail.js
+++ src/services/mail.js
@@ -1,6 +1,7 @@
 import mailTemplate from "../templates/mail.js";
+import { delay } from "../utils/index.js";
 
-export default function sendStudentMail(mail, studentName, studentEmail, sheetId) {
+export default async function sendStudentMail(mail, studentName, studentEmail, sheetId) {
   const template = mailTemplate(studentName, sheetId);
   var mailOptions = {
     from: process.env.EMAIL_USERNAME,
@@ -9,9 +10,11 @@ export default function sendStudentMail(mail, studentName, studentEmail, sheetId
     html: template,
   };
 
-  return mail.sendMail(mailOptions, (err, _info) => {
+  return mail.sendMail(mailOptions, async function(err, _info) {
     if (err) {
       console.log("Error sending email: ", err?.message);
+      await delay(60 * 1000); //1 minute
+      return await sendStudentMail(mail, studentName, studentEmail, sheetId);
     } else {
       console.log(`Email sended to student ${studentName}`);
     }
